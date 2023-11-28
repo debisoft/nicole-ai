7a1e2da83267bfd2fe7288e1791ccae71ad2aa12
hirakei1203 <hirano.keitaro.64x@gmail.com>
2023-11-28 15:34:56 +0900

    add/teams function

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.get('/', (req, res) => {
 	 handler(req, res, {	"public": "wwwroot"});
 })
 
-app.post('/api/', (req, res) => {
+// app.post('/api/', (req, res) => {
 
-	var response = request('POST', 'https://debisoft-magic.hf.space/api/predict', {
-		json: {data: [req.body.data]},
-	});
-	res.setHeader('Content-Type', 'application/json');
-  res.send(response.getBody('utf8'));
-})
+// 	var response = request('POST', 'https://debisoft-magic.hf.space/api/predict', {
+// 		json: {data: [req.body.data]},
+// 	});
+// 	res.setHeader('Content-Type', 'application/json');
+//   res.send(response.getBody('utf8'));
+// })
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
